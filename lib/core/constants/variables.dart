74414f2aa6820511a18ecd9938f33102beda70e5
TomoNx <tomosw07@gmail.com>
2024-08-26 18:30:41 +0700

    first commit

---
+++ lib/core/constants/variables.dart
@@ -0,0 +1,4 @@
+class Variables {
+  static const String baseUrl = 'http://192.168.100.5:80';
+  static const String imageBaseUrl = '$baseUrl/storage/products/';
+}
---
+++ lib/presentation/setting/pages/setting_page.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_pos_app/core/extensions/build_context_ext.dart';
+import 'package:flutter_pos_app/data/datasources/auth_local_datasource.dart';
+import 'package:flutter_pos_app/presentation/auth/pages/login_page.dart';
+import 'package:flutter_pos_app/presentation/setting/pages/sync_data_page.dart';
+
+import '../../../core/assets/assets.gen.dart';
+import '../../../core/components/menu_button.dart';
+import '../../../core/components/spaces.dart';
+import '../../home/bloc/logout/logout_bloc.dart';
+import 'manage_product_page.dart';
+
+class SettingPage extends StatefulWidget {
+  const SettingPage({super.key});
+
+  @override
+  State<SettingPage> createState() => _SettingPageState();
+}
+
+class _SettingPageState extends State<SettingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Setting'),
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                children: [
+                  MenuButton(
+                    iconPath: Assets.images.manageProduct.path,
+                    label: 'Kelola Produk',
+                    onPressed: () => context.push(const ManageProductPage()),
+                    isImage: true,
+                  ),
+                  const SpaceWidth(15.0),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                children: [
+                  MenuButton(
+                    iconPath: Assets.images.managePrinter.path,
+                    label: 'Sinkronisasi Data',
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const SyncDataPage()));
+                    },
+                    isImage: true,
+                  ),
+                ],
+              ),
+            ),
+            const SpaceHeight(60),
+            const Divider(),
+            BlocConsumer<LogoutBloc, LogoutState>(
+              listener: (context, state) {
+                state.maybeMap(
+                  orElse: () {},
+                  success: (_) {
+                    AuthLocalDatasource().removeAuthData();
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const LoginPage()));
+                  },
+                );
+              },
+              builder: (context, state) {
+                return ElevatedButton(
+                  onPressed: () {
+                    context.read<LogoutBloc>().add(const LogoutEvent.logout());
+                  },
+                  child: const Text('Logout'),
+                );
+              },
+            ),
+            const Divider(),
+          ],
+        ));
+  }
+}
